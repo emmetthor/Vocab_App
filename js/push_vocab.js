87696d20e0c00243b741e25521f6fa766c7af3ff
emmetthor <emmetthor@gmail.com>
2025-12-24 20:59:47 +0800

    debug push.js -> api/git_push.js

--- js/push_vocab.js
+++ js/push_vocab.js
@@ -12,11 +12,5 @@ async function pushVocab() {
 
 document.getElementById("test_btn").addEventListener("click", async () => {
     D.info("test_btn clicked");
-  try {
-    const res = await fetch("/api/push", { method: "POST" });
-    const data = await res.json();
-    console.log(data);
-  } catch (err) {
-    console.error(err);
-  }
+    pushVocab();
 });
