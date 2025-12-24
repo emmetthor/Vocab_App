9c7e81cd6dd8455a887530a1382cc962139db006
emmetthor <emmetthor@gmail.com>
2025-12-24 20:33:00 +0800

    test-token -> test_token

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ export const FETCH_URL = "https://raw.githubusercontent.com/emmetthor/Vocab_App/
 export const API_URL = "https://api.github.com/repos/Emmetthor/Vocab_App/commits?per_page=1";
 
 async function testToken() {
-  const res = await fetch("/api/test-token"); // 呼叫 Serverless Function
+  const res = await fetch("/api/test_token"); // 呼叫 Serverless Function
   const data = await res.json();
   console.log(data);
 }
