1ebefeb4d48cde388fe888efe26de4e3d33dd123
emmetthor <emmetthor@gmail.com>
2025-12-24 21:03:33 +0800

    REDEPLOY

--- js/push_vocab.js
+++ js/push_vocab.js
@@ -1,13 +1,13 @@
 import { D } from "./debug.js";
 
 async function pushVocab() {
-  try {
-    const res = await fetch("/api/git_push", { method: "POST" });
-    const data = await res.json();
-    console.log(data);
-  } catch (err) {
-    console.error(err);
-  }
+    try {
+        const res = await fetch("/api/git_push", { method: "POST" });
+        const data = await res.json();
+        console.log(data);
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 document.getElementById("test_btn").addEventListener("click", async () => {
