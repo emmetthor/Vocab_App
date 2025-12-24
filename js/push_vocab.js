5f43f34ab2b9585ff7ae2e2400d39422ee7a979e
emmetthor <emmetthor@gmail.com>
2025-12-24 20:53:30 +0800

    檢查 test_btn 是否按下

--- js/push_vocab.js
+++ js/push_vocab.js
@@ -1,3 +1,5 @@
+import { D } from "./debug";
+
 async function pushVocab() {
   try {
     const res = await fetch("/api/git_push", { method: "POST" });
@@ -9,6 +11,7 @@ async function pushVocab() {
 }
 
 document.getElementById("test_btn").addEventListener("click", async () => {
+    D.info("test_btn clicked");
   try {
     const res = await fetch("/api/push", { method: "POST" });
     const data = await res.json();
