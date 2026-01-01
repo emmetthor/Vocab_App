78ab1ab6946471de9b616aa62a3081efbb6ba5f3
Emmetthor <emmetthor@gmail.com>
2026-01-01 13:58:35 +0800

    使用官方 HF 使用方法嘗試

--- api/ask_ai.js
+++ api/ask_ai.js
@@ -1,32 +1,15 @@
-export const runtime = "nodejs";
-
-const { InferenceClient } = require("@huggingface/inference");
+import { InferenceClient } from "@huggingface/inference";
 
 const client = new InferenceClient(process.env.HF_TOKEN);
 
-export default async function handler(req, res) {
-    try {
-        if (req.method !== "POST") {
-        return res.status(405).json({ error: "Method not allowed" });
-        }
+const chatCompletion = await client.chatCompletion({
+    model: "meta-llama/Llama-3.1-8B-Instruct:novita",
+    messages: [
+        {
+            role: "user",
+            content: "What is the capital of France?",
+        },
+    ],
+});
 
-        const { prompt } = req.body ?? {};
-        if (!prompt) {
-        return res.status(400).json({ error: "Missing prompt" });
-        }
-
-        const result = await client.chatCompletion({
-        model: "meta-llama/Llama-3.1-8B-Instruct:novita",
-        messages: [{ role: "user", content: prompt }],
-        });
-
-        return res.status(200).json({
-        answer: result.choices[0].message.content,
-        });
-    } catch (err) {
-        console.error("ask_ai error:", err);
-        return res.status(500).json({
-        error: err.message ?? "Internal Server Error :( ",
-        });
-    }
-}
+console.log(chatCompletion.choices[0].message);
