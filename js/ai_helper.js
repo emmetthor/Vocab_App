77c787ada4b184cd23950530f7ac1c186b363ab5
emmetthor <emmetthor@gmail.com>
2026-01-01 17:48:35 +0800

    嘗試加入 ai 造句 prompt

--- js/ai_helper.js
+++ js/ai_helper.js
@@ -1,5 +1,25 @@
 async function askAI() {
-    const prompt = document.getElementById("input").value;
+    const vocab = document.getElementById("add-vocab").value;
+    const pos = document.getElementById("add-pos").value;
+    const meaning = document.getElementById("add-definition").value;
+    
+    if (!vocab) {
+        console.error("empty vocab");
+        return;
+    }
+
+    if (!pos) {
+        console.error("empty pos");
+        return;
+    }
+
+    if (!meaning) {
+        console.error("empty meaning");
+        return;
+    }
+
+    const prompt = `你是一個英文母語教師，現在想提升一位高中生的英文。有一個單字 [${vocab}]，它的詞性 [${pos}] 與它其中一個意思 [${meaning}]，請產生一個 1.語法正確 2.自然 3.符合該意思的用法 4.不使用複雜文法 5.簡短 的英語例句。請只輸出句子本身。謝謝。`
+
     const res = await fetch("/api/ask_ai", {
         method: "POST",
         body: JSON.stringify({ prompt }),
@@ -9,4 +29,4 @@ async function askAI() {
     console.log(data);
 }
 
-document.getElementById("send").addEventListener("click", askAI);
+document.getElementById("ai_example_btn").addEventListener("click", askAI);
